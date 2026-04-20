da9f5d606f69fe6eace72efa73cabbf02954e924
Varun Karankar <74638325+varunkarankar32@users.noreply.github.com>
2026-04-20 23:05:13 +0530

    Delete smms_app directory

--- smms_app/android/app/src/main/kotlin/com/smms/smms_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.smms.smms_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- smms_app/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:smms_app/main.dart';
-
-void main() {
-  testWidgets('App launches correctly', (WidgetTester tester) async {
-    await tester.pumpWidget(const SMSSApp());
-    expect(find.text('SMMS'), findsOneWidget);
-  });
-}
